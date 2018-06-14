a0dd5cc0e137e11dee8b9ba24297d5f149d4097f
Piyush <piyush.kumardav@gmail.com>
2018-06-14 21:46:45 +0530

    commit

---
+++ app/src/main/java/com/example/piyush/acm_alumni_app/login_page.java
@@ -0,0 +1,13 @@
+package com.example.piyush.acm_alumni_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
